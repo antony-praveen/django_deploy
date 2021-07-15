579bb394e12907d14d85ce5292b845c7354be9e3
antony-praveen <antony15898@gmail.com>
2021-07-15 13:17:28 +0530

    heroku adjustment

--- myhome/settings.py
+++ myhome/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -123,11 +123,15 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 #added mannually
 STATICFILES_DIRS = [
     BASE_DIR / "static",
     
 ]
+
+#heroku settings https://devcenter.heroku.com/articles/django-assets
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
