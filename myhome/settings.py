b0d876d716e339bb7ab2a268ce801072cbd62238
antony-praveen <antony15898@gmail.com>
2021-07-15 13:52:21 +0530

    static file change

--- myhome/settings.py
+++ myhome/settings.py
@@ -131,7 +131,7 @@ STATICFILES_DIRS = [
 ]
 
 #heroku settings https://devcenter.heroku.com/articles/django-assets
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
