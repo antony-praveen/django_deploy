81a5484be35a5a6ae01ddb712c3d31d9d5bbf46d
antony-praveen <antony15898@gmail.com>
2021-07-15 12:37:49 +0530

    add the domine name

--- myhome/settings.py
+++ myhome/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!k51k%nxo+0i4#!1xf7om36#)tf3us7zo#h!q&rdb(ncd&(v)h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','']
+ALLOWED_HOSTS = ['127.0.0.1','ecomshopp.herokuapp.com']
 
 
 # Application definition
