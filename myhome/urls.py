7ff7277805d8e284f0c4976a79ebc16201b3389b
antony-praveen <antony15898@gmail.com>
2021-07-15 12:31:02 +0530

    basic correctionn

--- myhome/urls.py
+++ myhome/urls.py
@@ -17,7 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from myroom import views
 
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index, name='index')
+
+    #url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    #url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
