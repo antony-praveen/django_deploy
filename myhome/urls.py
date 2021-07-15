da9663e335e7bbf81521f5f7b5486b3f6ef58a63
antony-praveen <antony15898@gmail.com>
2021-07-15 12:57:24 +0530

    static root

--- myhome/urls.py
+++ myhome/urls.py
@@ -17,12 +17,16 @@ from django.contrib import admin
 from django.urls import path
 from myroom import views
 
-
+from django.conf.urls.static import static
+from django.views.static import serve
+from django.conf.urls import url
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index, name='index')
 
-    #url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    #url(r'^media/(?P<path>.*)$', serve,{'document_root':  settings.MEDIA_ROOT}), 
     #url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
-]
+
+] +  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
