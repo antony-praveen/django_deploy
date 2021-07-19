dca4b5cbbc0e2d4abeb18229ba5cacf59ae3e471
antony-praveen <antony15898@gmail.com>
2021-07-19 09:54:39 +0530

    commit

--- bookapp/models.py
+++ bookapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class bookProduct(models.Model):
+    name = models.CharField(max_length = 200)
+    author = models.CharField(max_length = 200)
+    version = models.CharField(max_length = 100)
+    publishingYear = models.IntegerField()
+    publisherName = models.CharField(max_length = 200)
+    price = models.IntegerField()
+    image = models.CharField(max_length = 5000)
+
+    class Meta:
+        verbose_name = 'book deteail'
+
+        
