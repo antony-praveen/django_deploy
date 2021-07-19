41cc9d6f212912ed84f132cd197d956ee84c496a
antony-praveen <antony15898@gmail.com>
2021-07-19 07:36:10 +0530

    template settings reference link

--- bookshow/settings.py
+++ bookshow/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'bookshow.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'templates'],  # https://docs.djangoproject.com/en/3.2/howto/overriding-templates/
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
