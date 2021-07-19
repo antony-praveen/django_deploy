c22c4f63c1600a01caffc6f5c8b5bbb08fd4f316
antony-praveen <antony15898@gmail.com>
2021-07-19 07:48:49 +0530

    commit

--- bookshow/urls.py
+++ bookshow/urls.py
@@ -18,5 +18,5 @@ from django.urls import path
 from bookapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("/", views.home, name="home"),
+    path("", views.home, name="home"),
 ]
