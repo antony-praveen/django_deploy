f981f296c361852c100f41b307dd2901e3246178
antony-praveen <antony15898@gmail.com>
2021-07-20 12:22:09 +0530

    sort by acending order

--- bookapp/views.py
+++ bookapp/views.py
@@ -6,11 +6,11 @@ def home(request):
 
     # count the number of books have
     total_books = bookProduct.objects.count()
-    product_list = bookProduct.objects.all()
+    product_list = bookProduct.objects.all().order_by('price')
     # search functions 
     item = request.GET.get('item_name')
     if item != '' and item is not None:
-        product_list = product_list.filter(name__icontains=item)
+        product_list = product_list.filter(bookName__icontains=item)
 
     context = {'product_list':product_list,'total_books':total_books}
     return render(request, 'base.html', context,)
