40b69791398cd4c8b900ee57cb8387fe86962015
antony-praveen <antony15898@gmail.com>
2021-07-20 11:35:24 +0530

    search function

--- bookapp/views.py
+++ bookapp/views.py
@@ -3,6 +3,14 @@ from .models import bookProduct
 # Create your views here.
 
 def home(request):
+
+    # count the number of books have
+    total_books = bookProduct.objects.count()
     product_list = bookProduct.objects.all()
-    context = {product_list: 'product_list'}
-    return render(request, 'base.html', context)
+    # search functions 
+    item = request.GET.get('item_name')
+    if item != '' and item is not None:
+        product_list = product_list.filter(name__icontains=item)
+
+    context = {'product_list':product_list,'total_books':total_books}
+    return render(request, 'base.html', context,)
